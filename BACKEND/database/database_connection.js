d79e7e23593c657c909705b3837c9705da6ac247
cjoshi-systango <cjoshi@isystango.com>
2022-05-17 16:56:55 +0530

    fix:changing the database connection due to to many connection error

--- BACKEND/database/database_connection.js
+++ BACKEND/database/database_connection.js
@@ -2,10 +2,10 @@ mysql = require("mysql");
 require('dotenv').config()
 
 const connection = mysql.createConnection({
-    host: "sql11.freesqldatabase.com",
-    user: "sql11491580",
-    password: 'RX24ezevq9' ,
-    database : "sql11491580",
+    host: "sql4.freemysqlhosting.net",
+    user: "sql4492968",
+    password: 'm9Yc5Iwf9j' ,
+    database : "sql4492968",
     multipleStatements: true,
 })
 // console.log("bjusc");
