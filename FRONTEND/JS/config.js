197020ccc2c19445b6d065caa0080980ed04aadc
cjoshi-systango <cjoshi@isystango.com>
2022-05-20 19:10:13 +0530

    fix:correcting the port url

--- FRONTEND/JS/config.js
+++ FRONTEND/JS/config.js
@@ -18,8 +18,8 @@
 //     console.log(fetchUrl);
 //     return fetchUrl;
 // })()
-// let fetchUrl = `https://resolve-it-node-js-sql.herokuapp.com/`;
-let fetchUrl = "http://localhost:4001/";
+let fetchUrl = `https://resolve-it-node-js-sql.herokuapp.com/`;
+// let fetchUrl = "http://localhost:4001/";
 export{fetchUrl}
 // let fetchUrl = fetchPort();
 
