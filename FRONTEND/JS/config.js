18823c9b29517529d4c5420e9c300f0ff5d06ac7
cjoshi-systango <cjoshi@isystango.com>
2022-06-06 12:14:17 +0530

    fix:fixing heroku logs error

--- FRONTEND/JS/config.js
+++ FRONTEND/JS/config.js
@@ -1,23 +1,3 @@
-// let fetchUrl;
-
-
-// (async()=>
-// {
-//     await fetch("../PortJson").then(async (result) => {
-//         let response = await result.json()
-//         let data = response.table;
-//         console.log(data[0].port);
-//         let port = data[0].port
-        
-//     })
-//     .catch((e) => {
-//         console.error(e);
-//     })
-//     console.log("inside config");
-    
-//     console.log(fetchUrl);
-//     return fetchUrl;
-// })()
 let fetchUrl = `https://resolve-it-node-js-sql.herokuapp.com/`;
 // let fetchUrl = "http://localhost:4001/";
 export{fetchUrl}
